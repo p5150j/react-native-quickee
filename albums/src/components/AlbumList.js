2306dd86e8d0ea3c33c09243f509b4779cceaca7
Patrick <p5150j@gmail.com>
2016-09-21 19:11:39 -0600

    card component and rest call prep work

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail'
 
-const AlbumList = () => {
-	return (
-		<View>
-			<Text>ello!!</Text>
-		</View>
-		);
-};
+class AlbumList extends Component {
+
+  state = {
+    albums: []
+  };
+
+  componentWillMount() {
+    axios.get( '/api/music_albums' )
+      .then( responds => this.setState( {
+        albums: responds.data
+      } ) );
+  }
+
+  renderAlbums() {
+    return this.state.albums.map( album => <AlbumDetail key={ album.title } album={ album } /> );
+  }
+
+  render() {
+    return (
+    <View>
+      { this.renderAlbums() }
+    </View>
+    );
+  }
+
+}
 
 export default AlbumList;
--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
   return (
   <View style={ viewStyle }>
     <Text style={ textStyle }>
-      {props.headerText}
+      { props.headerText }
     </Text>
   </View>
   );
@@ -22,10 +22,13 @@ const styles = {
     backgroundColor: '#eee',
     justifyContent: 'center',
     alignItems: 'center',
-    height:60,
-    paddingTop:15,
+    height: 60,
+    paddingTop: 15,
     shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
     shadowOpacity: 0.2,
     elevation: 2,
     position: 'relative'
